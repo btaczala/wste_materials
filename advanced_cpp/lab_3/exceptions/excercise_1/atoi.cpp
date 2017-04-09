ae0910a4d3432194f07c20bfa005a48c022c199e
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-04-09 17:01:22 +0200

    [exception] 4

--- advanced_cpp/lab_3/exceptions/excercise_1/atoi.cpp
+++ advanced_cpp/lab_3/exceptions/excercise_1/atoi.cpp
@@ -3,6 +3,11 @@
 #include <string>
 #include <vector>
 
+void* operator new(size_t size) {
+    throw std::bad_alloc();
+    return nullptr;
+}
+
 int my_atoi(const std::string& str) {
     int value = std::atoi(str.c_str());
     if (value == 0 && str == "0") {
@@ -11,18 +16,25 @@ int my_atoi(const std::string& str) {
 
     if (value != 0) return value;
 
-    // throw std::invalid_argument("Bad argument");
-    throw 10;
+    throw std::invalid_argument("Bad argument");
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) return EXIT_FAILURE;
-
-    try {
-        std::cout << my_atoi(argv[1]) << std::endl;
-    } catch (int v) {
-        std::cerr << "Exception happened " << std::endl;
+    if (argc < 2) {
+        std::cerr << "" << std::endl;
+        return EXIT_FAILURE;
     }
 
+    int* p;
+    try {
+        p = new int;
+        std::cout << my_atoi(argv[1]) << std::endl;
+        delete p;
+    } catch (const std::exception& ex) {
+        std::cerr << "Exception happened " << ex.what() << std::endl;
+        delete p;
+    } catch (const std::bad_alloc& ex) {
+        std::cerr << "Bad Exception happened " << ex.what() << std::endl;
+    }
     return EXIT_SUCCESS;
 }

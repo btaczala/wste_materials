7d7fd9470c307bccb3db0733c4e97c7de467898f
Bartosz Taczala <bartosz.taczala@mobica.com>
2017-05-27 12:20:43 +0200

    Counting odd elements

--- second_semester/stl/ex2.cpp
+++ second_semester/stl/ex2.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-
 #include "ex2.hpp"
 
 TEST(ex2, simple) {
@@ -12,6 +11,55 @@ TEST(ex2, simple) {
     std::vector<int> randomized = my_std::randomize();
     randomized.at(15) = 100;
     EXPECT_EQ(my_std::findN(randomized, 100), 15);
+}
+
+TEST(ex3, insert_0_to_10_into_list) {
+    std::list<int> list;
+    my_std::insert_to_list(list);
+    std::list<int> expected{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    EXPECT_EQ(list, expected);
+}
+
+TEST(ex4, print_container_with_std_for_each) {
+    std::vector<std::string> toPrint{"test 1", "test 1"};
+    const std::string expectedOutput{"test 1, test 1"};
+
+    std::ostringstream ss;
+    my_std::print(toPrint, ss);
+    EXPECT_EQ(expectedOutput, ss.str());
+}
+
+TEST(ex5, string_to_upper) {
+    const std::string lowerCase{"lowerCaseString"};
+    EXPECT_EQ(my_std::upper(lowerCase), "LOWERCASESTRING");
+}
+
+TEST(ex6, mean_of_std_vector_int) {
+    std::vector<int> v;
+    iota_n(std::back_inserter(v) , 101, 0);
+
+    EXPECT_EQ(static_cast<int>(my_std::mean(v)), 50);
+    EXPECT_EQ(my_std::mean({}), 0.0f);
+}
+
+TEST(ex7_count_odd_elements, simple)
+{
+    EXPECT_EQ(my_std::count_odd({1, 3, 4}), 2);
+}
+
+TEST(ex7_count_odd_elements, proper)
+{
+    std::vector<int> v;
+    iota_n(std::back_inserter(v) , 101, 0);
+
+    EXPECT_EQ(my_std::count_odd(v), 50);
+    v.push_back(4);
+    EXPECT_EQ(my_std::count_odd(v), 50);
+}
+
+TEST(ex7_count_odd_elements, empty)
+{
+    EXPECT_EQ(my_std::count_odd({}), 0);
 }
 
 int main(int argc, char *argv[]) {
--- second_semester/stl/ex2.hpp
+++ second_semester/stl/ex2.hpp
@@ -3,23 +3,30 @@
 
 #include <algorithm>
 #include <cstdlib>
+#include <iostream>
+#include <list>
 #include <vector>
+#include <numeric>
 
-/*
-struct FindValue {
-    bool operator()(int value)
-    {
-    }
-};
-*/
+template <class OutputIterator, class Size, class Assignable>
+void iota_n(OutputIterator first, Size n, Assignable value) {
+    std::generate_n(first, n, [&value]() { return value++; });
+}
 
 namespace my_std {
 int findN(const std::vector<int>& v, int n) {
     // auto vit = std::find(v.begin(), v.end(), n);
-    auto vit =
-        std::find_if(v.begin(), v.end(), [n](int value) -> bool { return value == n; });
+    auto vit = std::find_if(v.begin(), v.end(),
+                            [n](int value) -> bool { return value == n; });
     if (vit == v.end()) return -1;
     return std::distance(v.begin(), vit);
+}
+
+void insert_to_list(std::list<int>& l) {
+    l = std::list<int>();
+    int counter = 0;
+    std::generate_n(std::back_inserter(l), 10,
+                    [&counter]() -> int { return counter++; });
 }
 
 std::vector<int> randomize() {
@@ -27,6 +34,32 @@ std::vector<int> randomize() {
     std::generate_n(std::back_inserter(random), std::rand() % 100, std::rand);
     return random;
 }
+
+void print(const std::vector<std::string> v, std::ostream& s) {
+    std::for_each(v.begin(), v.end() - 1,
+                  [&s](const std::string& str) { s << str << ", "; });
+    s << v[v.size() - 1];
+}
+
+std::string upper(const std::string& str) {
+    std::string t = str;
+    std::for_each(t.begin(), t.end(), [](char& t) { t = std::toupper(t); });
+    return t;
+}
+
+double mean(const std::vector<int>& v) {
+    if (v.empty()) return 0.0f;
+
+    int sum = 0;
+    std::for_each(v.begin(), v.end(), [&sum](int value) { sum += value; });
+    return static_cast<double>(sum) / static_cast<double>(v.size());
+}
+
+std::size_t count_odd(const std::vector<int>& v)
+{
+    return std::count_if(v.begin(), v.end(), [](int d) { return d % 2 != 0;});
+}
+
 }
 
 #endif /* end of include guard: EX2_HPP_GWJCVDEW */
